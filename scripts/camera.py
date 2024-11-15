5f3ef7d82880a61682712784a8fc97ce0fbeb853
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2024-11-16 01:47:00 +0530

    Changed camera structure

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,13 +1,27 @@
 import cv2
+from scripts.model import detect_objects  # Import your model's detection function
+
+scanning = False
+
+
+def start_scan():
+    global scanning
+    scanning = True
+
 
 def gen_frames():
+    global scanning
     camera = cv2.VideoCapture(0)  # Use 0 for the default camera
     while True:
         success, frame = camera.read()  # Read the camera frame
         if not success:
             break
         else:
+            if scanning:
+                # Detect objects and draw bounding boxes
+                frame = detect_objects(frame)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera.release()
